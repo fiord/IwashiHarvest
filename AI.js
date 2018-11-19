9359e1b97d5f82ee326de0435d2973a26bbed6cf
DESKTOP-967GIOI\hyoga <hyoga_quasar@yahoo.co.jp>
2018-11-19 16:23:46 +0900

    add それっぽいやつ

--- AI.js
+++ AI.js
@@ -7,78 +7,185 @@ class AI {
     getDist(x1, y1, x2, y2) {
         return Math.abs(x1-x2) + Math.abs(y1-y2);
     }
+    
+    iwashiMove(iwashiMap, maps, player, H, W) {
+        var distanceMap = new Array(H);
+        for(let i = 0; i < H; i++)  distanceMap[i] = new Array(W).fill(H * W);
+        var queue = [];
+        const dx = [0, 1, 0, -1, 0];
+        const dy = [-1, 0, 1, 0, 0];
+        
+        // player
+        if(player.paralyzed === 0) {
+            distanceMap[player.y][player.x] = 0;
+            queue.push({x: player.x, y: player.y});
+        }
+        // iwashi
+        for(let i = 0; i < H; i++) {
+            for(let j = 0; j < W; j++) {
+                if(iwashiMap[i][j] > 0) {
+                    distanceMap[i][j] = 0;
+                    queue.push({x: j, y: i});
+                }
+                else if(player.pos == {x: j, y: i}) {
+                    distanceMap[i][j] = 0;
+                    queue.push({x: j, y: i});
+                }
+            }
+        }
+    
+        while(queue.length > 0) {
+            var pos = queue.shift();
+            for(let i = 0; i < 4; i++) {
+                let nx = pos.x + dx[i];
+                let ny = pos.y + dy[i];
+                if(maps[ny][nx] === "#")  continue;
+                if(distanceMap[ny][nx] > distanceMap[pos.y][pos.x] + 1) {
+                    distanceMap[ny][nx] = distanceMap[pos.y][pos.x] + 1;
+                    queue.push({x: nx, y: ny});
+                }
+            }
+        }
+    
+        var nextIwashiMap = new Array(H);
+        for(let i = 0; i < H; i++)  nextIwashiMap[i] = new Array(W).fill(0);
+        
+        for(let i = 0; i < H; i++) {
+            for(let j = 0; j < W; j++) {
+                if(iwashiMap[i][j] > 0) {
+                    if(nextIwashiMap[i][j] > 0) {
+                        nextIwashiMap[i][j] += iwashiMap[i][j];
+                    }
+                    else {
+                            for(let k = 0; k < 5; k++) {
+                            let ni = i + dy[k];
+                            let nj = j + dx[k];
+                            if(maps[ni][nj] === "#")    continue;
+                            if(distanceMap[i][j] > distanceMap[ni][nj] || k == 4) {
+                                nextIwashiMap[ni][nj] += iwashiMap[i][j];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+        return nextIwashiMap;
+    }
 
     run(input) {
+        input = input.split("\n");
         const dx = [0, 1, 0, -1, 0];
         const dy = [1, 0, -1, 0, 0];
         const str = "SENWX";
         
         var arr = input[0].split(" ");
-        var H = Number(arr[0]), W = Number(arr[1]), T = Number(arr[2]), curT = Number(arr[3]);
+        var H = Number(arr[0]), W = Number(arr[1]), T = Number(arr[2]), N = Number(arr[3]);
         arr = input[1].split(" ");
         var player = {
             x: Number(arr[1]),
             y: Number(arr[0]),
             paralyzed: Number(arr[2])
         };
-        
-        if(player.paralyzed > 0) {
-            return "I hate math.";
+
+        var maps = new Array(H);
+        for(let i = 0; i < H; i++) {
+            maps[i] = input[i + 2];
         }
+        var iwashi = new Array(N);
+        for(let i = 0; i < N; i++) {
+            let line = input[i + 2 + H].split(" ");
+            iwashi[i] = {
+                x: parseInt(line[0]),
+                y: parseInt(line[1]),
+                t: parseInt(line[2])
+            };
+        }
+
         var iwashiMap = new Array(H);
-        for(let i = 0; i < H; i++) {
-            iwashiMap[i] = input[i + 2].split(" ").map((v) => Number(v));
+        for(let i = 0; i < H; i++)  iwashiMap[i] = new Array(W).fill(0);
+        var iwashiIdx = 0;
+        iwashi.sort((a, b) => (a.t-b.t));
+        while(iwashiIdx < N && iwashi[iwashiIdx].t === 0) {
+            iwashiMap[iwashi[iwashiIdx].y][iwashi[iwashiIdx.x]]++;
         }
-        
-        // 各マスからスコアマップを作成
-        var scoreMap = new Array(H);
-        for(let i = 0; i < H; i++)  scoreMap[i] = new Array(W).fill(0);
-        for(let i = 0; i < H; i++) {
-            for(let j = 0; j < W; j++) {
-                if(iwashiMap[i][j] > 0) {
-                    let iwashiValue = iwashiMap[i][j] * 3;
-                    if(iwashiValue > 5) {
-                        continue;
-                    }
-                    let addMap = new Array(H);
-                    for(let i = 0; i < H; i++)  addMap[i] = new Array(W).fill(0);
-                    let queue = [{x: j, y: i}];
-                    addMap[i][j] = iwashiValue;
-                    while(queue.length > 0) {
-                        let pos = queue.shift();
-                        for(let k = 0; k < 4; k++) {
-                            let nx = pos.x + dx[k];
-                            let ny = pos.y + dy[k];
-                            if(!this.isInside(nx, ny, W, H))    continue;
-                            if(iwashiMap[ny][nx] < 0)   continue;
-                            if(addMap[ny][nx] != 0) continue;
-                            addMap[ny][nx] += addMap[pos.y][pos.x] * 0.5;
-                            queue.push({x: nx, y: ny});
-                        }
-                    }
-                    for(let y = 0; y < H; y++) {
-                        for(let x = 0; x < W; x++) {
-                            scoreMap[y][x] += addMap[y][x];
+
+        let ret = "";
+        let turn = 0;
+        while(turn++ < N) {
+            if(player.paralyzed > 0) {
+                ret += "A";
+            }
+            else {
+                // 各マスからスコアマップを作成
+                let scoreMap = new Array(H);
+                for(let i = 0; i < H; i++)  scoreMap[i] = new Array(W).fill(0);
+                for(let i = 0; i < H; i++) {
+                    for(let j = 0; j < W; j++) {
+                        if(iwashiMap[i][j] > 0) {
+                            let iwashiValue = iwashiMap[i][j] * 3;
+                            if(iwashiValue > 5) {
+                                iwashiValue = -10;
+                            }
+                            let addMap = new Array(H);
+                            for(let i = 0; i < H; i++)  addMap[i] = new Array(W).fill(0);
+                            let queue = [{x: j, y: i}];
+                            addMap[i][j] = iwashiValue;
+                            while(queue.length > 0) {
+                                let pos = queue.shift();
+                                for(let k = 0; k < 4; k++) {
+                                    let nx = pos.x + dx[k];
+                                    let ny = pos.y + dy[k];
+                                    if(!this.isInside(nx, ny, W, H))    continue;
+                                    if(maps[ny][nx] === "#")   continue;
+                                    if(addMap[ny][nx] !== 0) continue;
+                                    addMap[ny][nx] += addMap[pos.y][pos.x] * 0.5;
+                                    queue.push({x: nx, y: ny});
+                                }
+                            }
+                            for(let y = 0; y < H; y++) {
+                                for(let x = 0; x < W; x++) {
+                                    scoreMap[y][x] += addMap[y][x];
+                                }
+                            }
                         }
                     }
                 }
+                // 一番スコアが高い方向へ移動
+                let to = 4;
+                for(let i = 4; i >= 0; i--) {
+                    let nx = player.x + dx[i];
+                    let ny = player.y + dy[i];
+                    if(!this.isInside(nx, ny, W, H)) {
+                        continue;
+                    }
+                    if(maps[ny][nx] === "#")   continue;
+                    if(scoreMap[ny][nx] >= scoreMap[player.y + dy[to]][player.x + dx[to]]) {
+                        to = i;
+                    }
+                }
+                ret += str[to];
+                player.x += dx[i];
+                player.y += dy[i];
             }
-        }
 
-        // 一番スコアが高い方向へ移動
-        let to = 4;
-        for(let i = 4; i >= 0; i--) {
-            let nx = player.x + dx[i];
-            let ny = player.y + dy[i];
-            if(!this.isInside(nx, ny, W, H)) {
-                continue;
+            // iwashimove
+            iwashiMap = this.iwashiMove(iwashiMap, maps, player, H, W);
+
+            
+
+            if(player.paralyzed > 0) {
+                player.paralyzed--;
             }
-            if(iwashiMap[ny][nx] < 0)   continue;
-            if(scoreMap[ny][nx] >= scoreMap[player.y + dy[to]][player.x + dx[to]]) {
-                to = i;
+            else if(0 < iwashiMap[player.y][player.x] && iwashiMap[player.y][player.x] <= 5){
+                iwashiMap[player.y][player.x] = 0;
+            }
+            else if(iwashiMap[player.y][player.x] > 5) {
+                player.paralyzed += 5;
             }
         }
-        return str[to];
+        return ret;
     }
 }
 
